4f7140870016ff59f6b689eedf75562bf9f1c5ac
ibrvhimv <103732028+ibrvhimv@users.noreply.github.com>
2022-05-12 22:07:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,25 @@
 //
 //  Created by Ibrahima Ndiaye on 15/04/2022.
 //
-
 #include <stdio.h>
+#include "metro.h"
 
-int main(void) {
-  
-  
+int main(void)
+{
+  FILE* file =NULL;
+  file=fopen("/Users/ibrahimandiaye/Documents/INFO/itinerairemetro/itinerairemetro/metro.csv","r");
+  LIST tab[34];
+  if(file != NULL)
+  {
+    loadListTab(tab, file);
+    fclose(file);
+  }
+  else
+  {
+    printf("Impossible d'ouvrir le fichier \n ");
+  }
+  printList(tab[33]);
 
   return 0;
 }
+
