0a033adfd4ae058bb55a78b89b1f32621fb8c758
ibrvhimv <103732028+ibrvhimv@users.noreply.github.com>
2022-05-13 09:52:39 +0200

    Update metro.h

--- metro.h
+++ metro.h
@@ -44,48 +44,9 @@ void appendToListSTation(LIST* list, STATION station);
 void loadtabTemps(double *tab, char* str , int i);
 void printList(LIST list);
 void loadStation(STATION* station, char* str ,double *tab , int i);
-void loadStationRER(STATION* station, char* str);
-void loadrer(LIST *list,char* str , FILE* file);
 void rewind(FILE *stream);
-void loadmetro1a(LIST *list,char* str , FILE* file);
-void loadmetro1b(LIST *list,char* str , FILE* file);
-void loadmetro2a(LIST *list,char* str , FILE* file);
-void loadmetro2b(LIST *list,char* str , FILE* file);
-void loadmetro3a(LIST *list,char* str , FILE* file);
-void loadmetro3b(LIST *list,char* str , FILE* file);
-void loadmetro3bisa(LIST *list,char* str , FILE* file);
-void loadmetro3bisb(LIST *list,char* str , FILE* file);
-void loadmetro4a(LIST *list,char* str , FILE* file);
-void loadmetro4b(LIST *list,char* str , FILE* file);
-void loadmetro5a(LIST *list,char* str , FILE* file);
-void loadmetro5b(LIST *list,char* str , FILE* file);
-void loadmetro6a(LIST *list,char* str , FILE* file);
-void loadmetro6b(LIST *list,char* str , FILE* file);
-void loadmetro7a(LIST *list,char* str , FILE* file);
-void loadmetro7b(LIST *list,char* str , FILE* file);
-void loadmetro7bisa(LIST *list,char* str , FILE* file);
-void loadmetro7bisb(LIST *list,char* str , FILE* file);
-void loadmetro8a(LIST *list,char* str , FILE* file);
-void loadmetro8b(LIST *list,char* str , FILE* file);
-void loadmetro9a(LIST *list,char* str , FILE* file);
-void loadmetro9b(LIST *list,char* str , FILE* file);
-void loadmetro10a(LIST *list,char* str , FILE* file);
-void loadmetro10b(LIST *list,char* str , FILE* file);
-void loadmetro11a(LIST *list,char* str , FILE* file);
-void loadmetro11b(LIST *list,char* str , FILE* file);
-void loadmetro12a(LIST *list,char* str , FILE* file);
-void loadmetro12b(LIST *list,char* str , FILE* file);
-void loadmetro13a(LIST *list,char* str , FILE* file);
-void loadmetro13b(LIST *list,char* str , FILE* file);
-void loadmetro13c(LIST *list,char* str , FILE* file);
-void loadmetro13d(LIST *list,char* str , FILE* file);
-void loadmetro14a(LIST *list,char* str , FILE* file);
-void loadmetro14b(LIST *list,char* str , FILE* file);
 void loadListTab(LIST *tab , FILE *file);
-
-
-
-
-
-
+void loadmetroX(LIST *list,char* str , FILE* file , char a0 ,char b1 ,char c2);
+void loadmetroXX(LIST *list,char* str , FILE* file , char a0 ,char b1 ,char c3);
+void loadmetroXbis(LIST *list,char* str , FILE* file , char a0 ,char b1 ,char c3);
 #endif /* metro_h */
